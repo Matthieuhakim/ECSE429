642c6cd2f46513a941850a47404ceb0345df9136
Matthieuhakim <77667703+Matthieuhakim@users.noreply.github.com>
2024-10-05 19:04:33 -0400

    Modify file structure

---
+++ PartA/src/test/java/TodosTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TodosTest {
+
+    @Test
+    public void sampleTest() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+
+}
